a3220df1c4ac65dcd50d0d4fc1b362c194765360
marktodman <drmtodman@gmail.com>
2022-03-24 17:11:29 +0000

    Calculate surplus data

--- run.py
+++ run.py
@@ -72,8 +72,15 @@ def calculate_surplus_data(sales_row):
     """
     print("Calculating surplus data...\n")
     stock = SHEET.worksheet("stock").get_all_values()
-    stock_row = stock.pop()
-    print(stock_row)
+    stock_row = stock[-1]
+    
+    surplus_data = []
+    for stock, sales in zip(stock_row, sales_row):
+        surplus = int(stock) - sales
+        surplus_data.append(surplus)
+    
+    
+    return surplus_data
 
 def main():
     """
@@ -83,7 +90,8 @@ def main():
     sales_data = [int(num) for num in data]
     print(sales_data)
     update_sales_worksheet(sales_data)
-    calculate_surplus_data(sales_data)
+    new_surplus_data = calculate_surplus_data(sales_data)
+    print(new_surplus_data)
 
 print("Welcome to Love Sandwiches Data Automation")
 main()
