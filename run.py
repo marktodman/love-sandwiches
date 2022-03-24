d2121a6883a01c03bceb633134eea78eeb28176c
marktodman <drmtodman@gmail.com>
2022-03-24 17:19:56 +0000

    Update surplus worksheet

--- run.py
+++ run.py
@@ -56,7 +56,7 @@ def validate_data(values):
 
 def update_sales_worksheet(data):
     """
-    Update sales worksheet, add new with the data provided.
+    Update sales worksheet, add new row with the data provided.
     """
     print("Updating sales worksheet...\n")
     sales_worksheet = SHEET.worksheet("sales")
@@ -79,8 +79,16 @@ def calculate_surplus_data(sales_row):
         surplus = int(stock) - sales
         surplus_data.append(surplus)
     
-    
     return surplus_data
+
+def update_surplus_worksheet(new_surplus_data):
+    """
+    Update surplus worksheet, add new row with the data provided.
+    """
+    print("Updating surplus worksheet...\n")
+    surplus_worksheet = SHEET.worksheet("surplus")
+    surplus_worksheet.append_row(new_surplus_data)
+    print("Surplus worksheet updated successfully.\n")
 
 def main():
     """
@@ -88,10 +96,9 @@ def main():
     """
     data = get_sales_data()
     sales_data = [int(num) for num in data]
-    print(sales_data)
     update_sales_worksheet(sales_data)
     new_surplus_data = calculate_surplus_data(sales_data)
-    print(new_surplus_data)
+    update_surplus_worksheet(new_surplus_data)
 
 print("Welcome to Love Sandwiches Data Automation")
 main()
